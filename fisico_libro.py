cf00a7b8c8f32a0919e61dd0a16f5dfab974c666
pablo <pyr4@alu.ua.es>
2026-03-14 11:06:12 +0100

    He cambiado los atributos self.estanteria y self.seccion a atributos protegidos (self._estanteria y self._seccion)

--- fisico_libro.py
+++ fisico_libro.py
@@ -3,13 +3,13 @@ from libro import Libro
 class Fisico(Libro):
     def __init__(self, titulo, autor, anyo, estanteria, seccion, estado="Excelente"):
         super().__init__(titulo, autor, anyo)
-        self.estanteria = estanteria
-        self.seccion = seccion
+        self._estanteria = estanteria # Atributo protegido por seguridad
+        self._seccion = seccion # Atributo protegido por seguridad
         self.estado = estado
 
     def mostrar_info(self):
         info_fisico = super().mostrar_info()
-        return f"{info_fisico} | Ubicación: Estantería {self.estanteria}, {self.seccion} | Estado: {self.estado}"
+        return f"{info_fisico} | Ubicación: Estantería {self._estanteria}, {self._seccion} | Estado: {self.estado}"
 
     def reportar_estado(self, nuevo_estado):
         self.estado = nuevo_estado
