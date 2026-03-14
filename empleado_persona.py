05a7d9d91f702010fcf8c7ed5d242e852bf1a85b
pablo <pyr4@alu.ua.es>
2026-03-14 11:01:45 +0100

    He cambiado el atributo self.id_empleado a un atributo privado (self.__id_empleado) y el atributo self.puesto a un atributo protegido (self._puesto)

--- empleado_persona.py
+++ empleado_persona.py
@@ -1,23 +1,25 @@
+from persona import Persona
+
 class Empleado(Persona):
     def __init__(self, dni, nombre, apellido, edad, id_empleado, puesto):
         super().__init__(dni, nombre, apellido, edad)
-        self.id_empleado = id_empleado
-        self.puesto = puesto.lower()
+        self.__id_empleado = id_empleado # Atributo privado. Dato únicamente de la biblioteca
+        self._puesto = puesto.lower() # Atributo protegido, ya que es el cargo del ususario
         self.libros_gestionados = 0
 
     def gestionar_registro(self, biblioteca, libro):
         permisos_validos = ['bibliotecario', 'administrador']
 
-        if self.puesto in permisos_validos:
+        if self._puesto in permisos_validos:
             biblioteca.agregar_libro(libro)
             self.libros_gestionados += 1
-            print(f"El empleado {self.nombre} (ID: {self.id_empleado}) ha registrado el libro '{libro.titulo}'.")
+            print(f"El empleado {self.nombre} (ID: {self.__id_empleado}) ha registrado el libro '{libro.titulo}'.")
             print(f"Total gestionado por este empleado: {self.libros_gestionados}")
         else:
-            print(f"ACCESO DENEGADO: El puesto de '{self.puesto}' no tiene permisos para modificar el inventario.")
+            print(f"ACCESO DENEGADO: El puesto de '{self._puesto}' no tiene permisos para modificar el inventario.")
 
     def mostrar_info(self):
-        return (f"EMPLEADO #{self.id_empleado}\n"
+        return (f"EMPLEADO #{self.__id_empleado}\n"
                 f"Nombre: {self.nombre} {self.apellido}\n"
-                f"Cargo: {self.puesto}\n"
+                f"Cargo: {self._puesto}\n"
                 f"{self.libros_gestionados} libros procesados.")
