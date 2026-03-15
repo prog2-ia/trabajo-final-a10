03fe714412ed0777e863df0b2ff2acc71d180597
pablo <pyr4@alu.ua.es>
2026-03-15 20:23:08 +0100

    He añadido comentarios al código

--- clases/libro_local/fisico_libro.py
+++ clases/libro_local/fisico_libro.py
@@ -7,10 +7,10 @@ class Fisico(Libro):
         self._seccion = seccion # Atributo protegido por seguridad
         self.estado = estado
 
-    def mostrar_info(self):
-        info_fisico = super().mostrar_info()
+    def mostrar_info(self): # Método para mostrar la info de un libro físico #
+        info_fisico = super().mostrar_info() # Extendemos el método original
         return f"{info_fisico} | Ubicación: Estantería {self._estanteria}, {self._seccion} | Estado: {self.estado}"
 
-    def reportar_estado(self, nuevo_estado):
+    def reportar_estado(self, nuevo_estado): # Método para actualizar el estado de un libro físico
         self.estado = nuevo_estado
         print(f"El estado de '{self.titulo}' ha sido actualizado a: {self.estado}")
