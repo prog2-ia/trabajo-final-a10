dfe112ee4f51b4c726b41951d126db7a82a3f956
pablo <pyr4@alu.ua.es>
2026-03-15 20:21:40 +0100

    He añadido comentarios al código

--- clases/genero/genero.py
+++ clases/genero/genero.py
@@ -1,27 +1,26 @@
-#Clase padre genero
 class Genero:
     def __init__(self,nombre):
         self.nombre=nombre
-        self.libros=[]
+        self.libros=[] # Lista vacía para guardar libros de ese género
 
-    def agregar(self,libro,biblioteca):
-        if libro not in biblioteca.libros:
+    def agregar(self,libro,biblioteca): # Método para agregar un libro de ese género
+        if libro not in biblioteca.libros: # Verifica primero que el libro exista en la biblioteca
             print('El libro_local no existe en la biblioteca')
             return
-        for i in self.libros:
+        for i in self.libros: # Verifica si el libro ya está en la lista de su género
             if i.titulo==libro.titulo:
                 print('El libro_local ya existe en este género')
                 return
-        self.libros.append(libro)
+        self.libros.append(libro) # Si todo lo demás no se cumple, añade el libro a la lista
         libro.genero=self
 
-    def listar(self):
-        if not self.libros:
+    def listar(self): # Método para mostrar por pantalla los libros de la lista género
+        if not self.libros: # Si no hay libros en la lista, mensaje de error
             print("El género está vacío")
-        else:
-            for libro in self.libros:
+        else: # Si hay libros...
+            for libro in self.libros: # Recorre elemento a elemento de la lista y los va imprimiendo
                 print(libro)
 
-    def mostrar_info_genero(self):
+    def mostrar_info_genero(self): # Método para mostrar información sobre un género específico
         print('Género: ',self.nombre)
         print('Número de libros: ',len(self.libros))
