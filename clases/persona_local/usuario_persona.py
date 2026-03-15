2273e38c0230a93e8f193afa98bc999f8a5b8c01
pablo <pyr4@alu.ua.es>
2026-03-15 20:23:40 +0100

    He añadido comentarios al código

--- clases/persona_local/usuario_persona.py
+++ clases/persona_local/usuario_persona.py
@@ -1,18 +1,21 @@
-class Usuario(Persona):
-    def __init__(self,dni,nombre,apellido,edad): #Crear un usuario
-        super().__init__(dni,nombre,apellido,edad)
-        self.__prestados = []  # Atributo privado. Solo el usuario puede gestionar su lista
+from persona_local import Persona
 
-    def prestar_libro(self,libro): #Prestar el libro dependiendo de que si este esté disponible
-        if libro.disponible:
-            libro.prestar()
+class Usuario(Persona):
+    def __init__(self,dni,nombre,apellido,edad):
+        super().__init__(dni,nombre,apellido,edad)
+        # Atributo privado. Solo el usuario puede gestionar su lista
+        self.__prestados = [] # Lista de los libros que tiene el usuario en posesión porque la biblioteca se los ha prestado
+
+    def prestar_libro(self,libro): # Método para solicitar un libro a la biblioteca
+        if libro.disponible: # Verifica que el libro esté disponible (True)
+            libro.prestar() # Se ejecuta el método de libro_local
             self.__prestados.append(libro)
-        else:
+        else: # Si disponible = False no se puede solicitar
             print('No se puede prestar el libro.')
 
-    def devolver_libro(self,libro): #Devolver el libro dependiendo de que si este se encuentra entre los prestados
-        if libro in self.__prestados:
-            libro.devolver()
+    def devolver_libro(self,libro): # Método para que el usuario devuelva un libro a la biblioteca
+        if libro in self.__prestados: # Verifica que el libro lo tenga el usuario
+            libro.devolver() # Se ejecuta el método de libro_local
             self.__prestados.remove(libro)
         else:
             print('Libro incorrecto.')
