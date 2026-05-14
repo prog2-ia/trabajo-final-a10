0d4fff81eea22d1a661099b3b725d18ead81c7d0
oh is YG <gjy1@alu.ua.es>
2026-05-14 23:21:00 +0200

    modificada para adaptar la nueva clase prestamos

--- clases/persona_local/usuario_persona.py
+++ clases/persona_local/usuario_persona.py
@@ -1,21 +1,24 @@
 from clases.persona_local.persona_local import Persona
+from clases.prestamo.prestamos import Prestamos
 
 class Usuario(Persona):
     def __init__(self,dni,nombre,apellido,edad):
         super().__init__(dni,nombre,apellido,edad)
         # Atributo privado. Solo el usuario puede gestionar su lista
-        self.__prestados = [] # Lista de los libros que tiene el usuario en posesión porque la biblioteca se los ha prestado
+        self.__prestamos=[] # Lista de los libros que tiene el usuario en posesión porque la biblioteca se los ha prestado
 
     def prestar_libro(self,libro): # Método para solicitar un libro a la biblioteca
         if libro.disponible: # Verifica que el libro esté disponible (True)
             libro.prestar() # Se ejecuta el método de libro_local
-            self.__prestados.append(libro)
+            nuevo_prestamo=Prestamos(self,libro)
+            self.__prestamos.append(nuevo_prestamo)
         else: # Si disponible = False no se puede solicitar
             print('No se puede prestar el libro.')
 
     def devolver_libro(self,libro): # Método para que el usuario devuelva un libro a la biblioteca
-        if libro in self.__prestados: # Verifica que el libro lo tenga el usuario
-            libro.devolver() # Se ejecuta el método de libro_local
-            self.__prestados.remove(libro)
-        else:
-            print('Libro incorrecto.')
+        for pres in self.__prestamos:
+            if pres.libro==libro and pres.activo:
+                libro.devolver()
+                pres.finalizar_prestamo()
+                return
+        print('No se puede devolver el libro.')
