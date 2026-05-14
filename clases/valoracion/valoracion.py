bc7f311b65b2a23aa6aafafa1e65a4cc06b13e25
oh is YG <gjy1@alu.ua.es>
2026-05-14 23:21:28 +0200

    pequeñas modificaciones

--- clases/valoracion/valoracion.py
+++ clases/valoracion/valoracion.py
@@ -1,7 +1,6 @@
-class Valoracion: #No utilizado de momento
+class Valoracion:
     def __init__(self):
-        # Atributo privado para evitar la manipulación
-        self.__valoraciones = {} # Diccionario sobre las valoraciones de cada libro
+        self.valoraciones={} # Diccionario sobre las valoraciones de cada libro
 
     def valorar(self, biblioteca, libro, puntuacion): # Método que recibe la biblioteca para valorar un libro de esta
         if libro not in biblioteca.libros: # Comprueba que el libro exista dentro de la biblioteca proporcionada
@@ -12,9 +11,8 @@ class Valoracion: #No utilizado de momento
             print("La puntuación debe ser entre 0 y 10")
             return # Sale si la nota no es válida
 
-        self.__valoraciones[libro] = puntuacion # Si todo se cumple, agrega la valoración al diccionario
-        print(f"Valoración de {puntuacion} añadida a '{libro}'")
+        self.valoraciones[libro] = puntuacion # Si todo se cumple, agrega la valoración al diccionario
 
     def mostrar_valoraciones(self): # Método para mostrar las valoraciones
-        for libro in self.__valoraciones:
-            print(libro, ":", self.__valoraciones[libro])
+        for libro in self.valoraciones:
+            print(libro, ":", self.valoraciones[libro])
