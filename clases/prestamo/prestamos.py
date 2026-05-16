c1ca5dd175c00de54a3a219f09526e18bf88a4b6
oh is YG <gjy1@alu.ua.es>
2026-05-17 01:41:10 +0200

    Pequeñas modificaciones

--- clases/prestamo/prestamos.py
+++ clases/prestamo/prestamos.py
@@ -1,8 +1,8 @@
 class Prestamos:
-    def __init__(self, usuario, libro) -> None:
-        self.usuario = usuario
-        self.libro = libro
-        self.activo: bool = True
+    def __init__(self,usuario,libro) -> None:
+        self.usuario=usuario
+        self.libro=libro
+        self.activo:bool=True
 
     def finalizar_prestamo(self) -> None:
-        self.activo = False
+        self.activo=False
